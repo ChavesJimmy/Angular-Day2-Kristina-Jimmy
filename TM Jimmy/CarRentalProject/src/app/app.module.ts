f0bdd416848361218b995644a633d1f88883e476
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-11 11:57:13 +0200

    added contact form

--- TM Jimmy/CarRentalProject/src/app/app.module.ts
+++ TM Jimmy/CarRentalProject/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TM Jimmy/CarRentalProject/src/app/contact/contact.component.ts
+++ TM Jimmy/CarRentalProject/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  info = new FormGroup({
+    firstName: new FormControl('',Validators.required), 
+    lastName: new FormControl('',Validators.required), 
+    age: new FormControl('',Validators.required), 
+    email:new FormControl('',Validators.required),
+    sexe : new FormControl ('',Validators.required),
+    comments : new FormControl('', Validators.required)
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit(){
 
+    if(this.info.valid){
+      var a = this.info.value;
+      console.log(a)
+   }
+}
 }
